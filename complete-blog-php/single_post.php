24b27deb580d79d1504c04304bfa7ad8c14bcde6
Charan <61079036+Charan-sai-Myana@users.noreply.github.com>
2021-02-01 23:18:39 +0530

    Add files via upload

---
+++ complete-blog-php/single_post.php
@@ -0,0 +1,90 @@
+<?php/* * * * * * * * * * * * * * *
+* Returns a single post
+* * * * * * * * * * * * * * */
+function getPost($slug){
+	global $conn;
+	// Get single post slug
+	$post_slug = $_GET['post-slug'];
+	$sql = "SELECT * FROM posts WHERE slug='$post_slug' AND published=true";
+	$result = mysqli_query($conn, $sql);
+
+	// fetch query results as associative array.
+	$post = mysqli_fetch_assoc($result);
+	if ($post) {
+		// get the topic to which this post belongs
+		$post['topic'] = getPostTopic($post['id']);
+	}
+	return $post;
+}
+/* * * * * * * * * * * *
+*  Returns all topics
+* * * * * * * * * * * * */
+function getAllTopics()
+{
+	global $conn;
+	$sql = "SELECT * FROM topics";
+	$result = mysqli_query($conn, $sql);
+	$topics = mysqli_fetch_all($result, MYSQLI_ASSOC);
+	return $topics;
+}?>
+
+<?php  include('config.php'); ?>
+<?php  include('includes/public_functions.php'); ?>
+<?php 
+	if (isset($_GET['post-slug'])) {
+		$post = getPost($_GET['post-slug']);
+	}
+	$topics = getAllTopics();
+?>
+<?php include('includes/head_section.php'); ?>
+<title> <?php echo $post['title'] ?> | LifeBlog</title>
+</head>
+<body>
+<div class="container">
+	<!-- Navbar -->
+		<?php include( ROOT_PATH . '/includes/navbar.php'); ?>
+	<!-- // Navbar -->
+	
+	<div class="content" >
+		<!-- Page wrapper -->
+		<div class="post-wrapper">
+			<!-- full post div -->
+			<div class="full-post-div">
+			<?php if ($post['published'] == false): ?>
+				<h2 class="post-title">Sorry... This post has not been published</h2>
+			<?php else: ?>
+				<h2 class="post-title"><?php echo $post['title']; ?></h2>
+				<div class="post-body-div">
+					<?php echo html_entity_decode($post['body']); ?>
+				</div>
+			<?php endif ?>
+			</div>
+			<!-- // full post div -->
+			
+			<!-- comments section -->
+			<!--  coming soon ...  -->
+		</div>
+		<!-- // Page wrapper -->
+
+		<!-- post sidebar -->
+		<div class="post-sidebar">
+			<div class="card">
+				<div class="card-header">
+					<h2>Topics</h2>
+				</div>
+				<div class="card-content">
+					<?php foreach ($topics as $topic): ?>
+						<a 
+							href="<?php echo BASE_URL . 'filtered_posts.php?topic=' . $topic['id'] ?>">
+							<?php echo $topic['name']; ?>
+						</a> 
+					<?php endforeach ?>
+				</div>
+			</div>
+		</div>
+		<!-- // post sidebar -->
+	</div>
+</div>
+<!-- // content -->
+
+<?php include( ROOT_PATH . '/includes/footer.php'); ?>
